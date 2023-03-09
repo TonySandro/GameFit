3a5e76ab908885d8a0a3c1baff66f791c6cd1829
Tony Sandro <tonysduarte101@gmail.com>
2023-03-09 11:11:06 -0300

    feat: remove firebase configs

--- WEB/backend/src/services/firebase.js
+++ WEB/backend/src/services/firebase.js
@@ -1,13 +1,7 @@
 const firebase = require('firebase').default;
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAsg1NaEYzq_noxEyQAdWcxxJT2DOr5ef0",
-  authDomain: "hmranking.firebaseapp.com",
-  projectId: "hmranking",
-  storageBucket: "hmranking.appspot.com",
-  messagingSenderId: "998775467215",
-  appId: "1:998775467215:web:9e8658ca89ce91fa7f1bba",
-  measurementId: "G-VV88FMMBLR"
+
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
